f75a55f3b0819840733f53999a54a80eca9275b3
steven <>
2019-09-25 17:01:32 +0800

    update new file

---
+++ TableView+CollectionView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableView+CollectionView
+//
+//  Created by apple on 2019/9/25.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
